b884d0da7bc0f3e5f0fca9f117d8c64ddb85c6b6
Jordan Widjaja <jordanwidjaja@Jordans-MacBook-Air.local>
2019-03-24 17:29:11 -0400

    added initial SQL tables file

--- init_tables.sql
+++ init_tables.sql
@@ -14,8 +14,37 @@ create table products (
 	productID int NOT NULL AUTO_INCREMENT,
 	item_name varchar(100) NOT NULL,
 	current_stock_quantity int NOT NULL,
-	price float(24) NOT NULL,
+	price float(25) NOT NULL,
 	nutrition_facts varchar(1000) NOT NULL,
 	product_type varchar(100) NOT NULL
 );
 
+create table shopping_cart (
+	shopping_cartID int NOT NULL AUTO_INCREMENT,
+	userID int NOT NULL
+);
+
+create table cartItem (
+	cartID int NOT NULL AUTO_INCREMENT,
+	productID int NOT NULL,
+	quantity int NOT NULL,
+	shopping_cartID int NOT NULL
+);
+
+create table order (
+	orderID int NOT NULL AUTO_INCREMENT,
+	userID int NOT NULL,
+	shopping_cartID int NOT NULL,
+	addressID int NOT NULL,
+	total float(50) NOT NULL,
+	order_date datetime NOT NULL
+);
+
+create table address (
+	addressID int NOT NULL AUTO_INCREMENT,
+	userID int NOT NULL,
+	street varchar(100) NOT NULL,
+	city varchar(100) NOT NULL,
+	state varchar(2) NOT NULL,
+	zip int NOT NULL
+)
